86bd97091727747e6b9486ba284e9baf293ad581
Exarchiasghost <exarchiasghost@gmail.com>
2016-04-15 17:17:24 +0200

    probably colours is ready for submit. i need to submit to know if everything is ok

--- Beginner/Chef And Coloring/coloring chefs room.py
+++ Beginner/Chef And Coloring/coloring chefs room.py
@@ -4,13 +4,33 @@ roomsorcolor = 0
 
 f = open('output.txt','w')
 
+def most_common(lst):
+    return max(set(lst), key=lst.count)
+
+
+def configuratesomeshit(somelist):
+    keyshit = most_common(somelist)
+    paintjob = 0
+    print("we will paint with", keyshit)
+    for shit in somelist:
+        if shit == "R" or shit == "G" or shit=="B":
+            if shit == keyshit:
+                print("the color in the room is alrwady ", shit, " No action is necessary", sep="")
+            else:
+                print("the room is ", shit, " .We have to paint it ", keyshit, sep="")
+                paintjob+=1
+        else:
+            print("i found a piece of junk, it is my lucky one")
+    print("it took ", paintjob, " to paint all the rooms")
+    print(paintjob,sep="", end='\n', file=f)
+
 
 
 
 for line in fileinput.input("happyinput.in"):
     if firstlineiszero == 0:
         firstlineiszero +=1
-        print("there will be ", line, " cases!")
+        print("there will be ", line, " cases!", sep="", end='\n')
     else:
         firstlineiszero+=1
         print("the line ", firstlineiszero-1 ," is reffering to the ", line, " statement", sep="")
@@ -18,8 +38,11 @@ for line in fileinput.input("happyinput.in"):
             print("there will be ", line, " rooms to renovate", sep="")
             roomsorcolor = 1
         else:
-            print("color configuration will be ", line, sep="")
             roomsorcolor = 0
+            roomconfiguration = str(line)
+            list(roomconfiguration)
+            print("color configuration will be ", roomconfiguration, sep="")
+            configuratesomeshit(roomconfiguration)
 
 
 #print("Case #", str(unregisteredcase), ": ", number_string2, sep="")
@@ -29,5 +52,5 @@ for line in fileinput.input("happyinput.in"):
 
 
 f.close()
-print("That's all filks!")
+print("That's all folks!")
 
