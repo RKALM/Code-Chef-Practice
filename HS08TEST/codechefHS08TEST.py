485ad6b8139470f5c4d58afc7ccf6927404dfeba
Exarchiasghost <exarchiasghost@gmail.com>
2016-04-23 20:55:45 +0200

    i solved the calculations problem in HS08TEST. remains to fix the inputs and the outputs and it is ready :)

--- HS08TEST/codechefHS08TEST.py
+++ HS08TEST/codechefHS08TEST.py
@@ -1,13 +1,34 @@
-import sys
+#import sys
 
 amount = 0
 
-for line in sys.stdin:
-        amount = float(line)
-        if amount % 5 == 0:
-            print("the amount is divided by 5")
-            print(amount)
+def isdivby5(num):
+        if int(num)%5==0:
+                return True
         else:
-            amount = amount - 0,5
-            print("not divided by 5")
+                return False
+
+def withdraw(amount, account):
+        if float(amount) > float(account):
+                return account
+        else:
+                if float(account) > 0.5:
+                        account = float(account) - 0.5
+                        while float(amount) > 0:
+                                if isdivby5(float(amount)):
+                                        account = float(account) - float(amount)
+                                        return account
+                                        amount = 0
+                                else:
+                                        amount = float(amount) - 0.5
+                
+
+#for line in sys.stdin:
+#        amount = float(line)
+#        if amount % 5 == 0:
+#            print("the amount is divided by 5")
+#            print(amount)
+#        else:
+#            amount = amount - 0,5
+#            print("not divided by 5")
         
