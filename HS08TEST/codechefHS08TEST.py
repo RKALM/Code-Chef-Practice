0461efcb3cdece6a570866a48e371dca8c116dc4
Exarchiasghost <exarchiasghost@gmail.com>
2016-04-23 23:58:34 +0200

    HS08TEST almost ready

--- HS08TEST/codechefHS08TEST.py
+++ HS08TEST/codechefHS08TEST.py
@@ -1,6 +1,6 @@
-#import sys
+import sys
 
-amount = 0
+happylist = list()
 
 def isdivby5(num):
         if int(num)%5==0:
@@ -9,26 +9,21 @@ def isdivby5(num):
                 return False
 
 def withdraw(amount, account):
-        if float(amount) > float(account):
+        if float(amount) > float(account) or float(amount) < 5.5:
                 return account
         else:
                 if float(account) > 0.5:
-                        account = float(account) - 0.5
                         while float(amount) > 0:
                                 if isdivby5(float(amount)):
                                         account = float(account) - float(amount)
+                                        account = float(account) - 0.5
                                         return account
                                         amount = 0
                                 else:
                                         amount = float(amount) - 0.5
                 
 
-#for line in sys.stdin:
-#        amount = float(line)
-#        if amount % 5 == 0:
-#            print("the amount is divided by 5")
-#            print(amount)
-#        else:
-#            amount = amount - 0,5
-#            print("not divided by 5")
+for line in sys.stdin:
+        happylist = line.split()
+        print(withdraw(happylist[0],happylist[1]))
         
