225805175a153edd122ca890f6a6240e80a8d6c9
Exarchiasghost <exarchiasghost@gmail.com>
2017-01-08 16:52:49 +0100

    keep going
    small issue with the scope of the variables.
    I will find a solution easily.

--- FCTRL/happydivision.py
+++ FCTRL/happydivision.py
@@ -2,11 +2,10 @@ import sys
 
 itIsTheFirstLine = True
 arrayNList = list()
-counter = 0
-firstint = 0
 setN=0
 N1=0
 N2=0
+result = "Not any result right now"
 
 def n1N2BalanceChecker(N1,N2):
         if int(N1) <= int(N2):
@@ -16,15 +15,25 @@ def functionZ(N1,N2):
     if n1N2BalanceChecker(N1,N2):
         return float(N1) * float(N2)
 
-def firstLineDetection(inputForNInitiation):
+def firstLineCorrection(inputForNInitiation):
     itIsTheFirstLine = False
     setN = inputForNInitiation
     
 
 for N in sys.stdin:
+    print("itIsTheFirstLine is " + str(itIsTheFirstLine))
+    if itIsTheFirstLine == True:
+        firstLineCorrection(N)
+    else:
+        if N < setN:
+            result = str(functionZ(N,N+1))
+            print("the setN now is " + str(setN))
+            print ("the N now is " + str(N))
+ 
+        
 
 
-print(count)
+print(result)
 
 
 
