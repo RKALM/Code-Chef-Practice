f1b710c74ae78220b157f46d8faadd7cc088dedb
Exarchiasghost <exarchiasghost@gmail.com>
2017-01-08 17:13:08 +0100

    Doing something
    probably the day today will be only to finish with the Code chef python
    3 template.
    I guess i will solve the problem tomorrow but i have to stick bit around
    to complete the template and add comments and stuff :)

--- FCTRL/happydivision.py
+++ FCTRL/happydivision.py
@@ -7,15 +7,17 @@ N1=0
 N2=0
 result = "Not any result right now"
 
-def n1N2BalanceChecker(N1,N2):
-        if int(N1) <= int(N2):
+def n1N2BalanceChecker(N1):
+        if int(N1) > 0:
                 return True
             
 def functionZ(N1,N2):
-    if n1N2BalanceChecker(N1,N2):
+    if n1N2BalanceChecker(N1):
         return float(N1) * float(N2)
 
 def firstLineCorrection(inputForNInitiation):
+    global itIsTheFirstLine
+    global setN
     itIsTheFirstLine = False
     setN = inputForNInitiation
     
@@ -26,7 +28,7 @@ for N in sys.stdin:
         firstLineCorrection(N)
     else:
         if N < setN:
-            result = str(functionZ(N,N+1))
+            result = functionZ(int(N),int(N)+1)
             print("the setN now is " + str(setN))
             print ("the N now is " + str(N))
  
