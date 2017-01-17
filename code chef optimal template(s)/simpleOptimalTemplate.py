efb2f9470ca5c161663871fbae02df1532e5df9a
Exarchiasghost <exarchiasghost@gmail.com>
2017-01-17 04:15:02 +0100

    renaming and commenting the optimal template

--- code chef optimal template(s)/simpleOptimalTemplate.py
+++ code chef optimal template(s)/simpleOptimalTemplate.py
@@ -1,18 +1,21 @@
 import sys
 
-itIsTheFirstLine = True
-arrayNList = list()
-T=0
-result = "Not any result right now"
+itIsTheFirstLine = True     #This variable is about the input line. If the line is the first the variable itIsTheFirstLine is True.
+T=0     #The number of lines with usefull Data.
+result = "Not any result right now" #this is used for the result wich is printed in the end of the code with the default value.
 
+#the mainCheckFunction() function is doing the necessary checking before the mainCalculativFunction().
+#there is always something to check
 def mainCheckFunction(checkFunctionsProperty):
         if checkFunctionsProperty == True:
                 return True
-            
+
+#The function mainCalculativFunction() solves the logical problem of the exercise.         
 def mainCalculativFunction(inputProperty):
     if mainCheckFunction(True):
         return inputProperty
 
+#The function firstLineCorrection() changes the itIsTheFirstLine to False and stores the N to the variable T
 def firstLineCorrection(inputForNInitiation):
     global itIsTheFirstLine
     global T
