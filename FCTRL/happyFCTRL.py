ce1bd3fb60b6036493a81d7fd6d0149dd7d1f5c8
Exarchiasghost <exarchiasghost@gmail.com>
2017-01-17 17:29:55 +0100

    done with the technical limitations. Time for math.

--- FCTRL/happyFCTRL.py
+++ FCTRL/happyFCTRL.py
@@ -10,11 +10,13 @@ result = "Not any result right now"
 def functionZ(N1):
     reminder = 1.1
     i = 1
+    ZN1 = 1
+    ZN2 = 0
     while float(reminder) > 1.0:
         ZN1 = float(N1) / float(5^i)
         reminder = float(N1) - float(ZN1)
         i+=1
-        ZN2 =+ int(ZN1)
+        ZN2 = ZN2 + int(ZN1)
     return ZN2
         
     
@@ -31,12 +33,14 @@ for N in sys.stdin:
     if itIsTheFirstLine == True:
         firstLineCorrection(N)
     else:
-        if N <= setN:
+        if N < setN:
             result = functionZ(int(N))
             print("the setN now is " + str(setN))
             print ("the N now is " + str(N))
             print("The result is " + str(result))
         else:
+            result = functionZ(int(N))
+            print("The result is " + str(result))
             print("That's all folks!")
             #here i need to kill the function
         
