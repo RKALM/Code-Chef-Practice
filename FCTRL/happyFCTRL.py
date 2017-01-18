291ce935e04c2c89e0e5d0e09328713daabde02c
Exarchiasghost <exarchiasghost@gmail.com>
2017-01-18 21:46:22 +0100

    Ready for testing
    i left the debugging prints as comments.

--- FCTRL/happyFCTRL.py
+++ FCTRL/happyFCTRL.py
@@ -23,16 +23,15 @@ def functionZ(N1):
     
 
     while mainCheckFunction(float(N1),float(i)):
-        print("Now I will divine " + str(N1) + " with " + str(5**i))
+        #print("Now I will divine " + str(N1) + " with " + str(5**i))
         ZN1 = float(N1) / float(5**i)
-        print("the Z1 now is " + str(ZN1))
-        reminder = float(N1) - float(ZN1 * (5.0**float(i))) #the problem is still here.
-        #reminder = float(N1) % float(5**i) #the problem is about that I am using the reminder the wrong way.
-        print("the reminder now is " + str(reminder))
+        #print("the Z1 now is " + str(ZN1))
+        reminder = float(N1) - float(ZN1 * (5.0**float(i))) 
+        #print("the reminder now is " + str(reminder))
         i+=1
         ZN2 = ZN2 + int(ZN1)
-        print("I am thinking to print " + str(ZN2))
-    print("I made my mind and I will print " + str(ZN2))
+        #print("I am thinking to print " + str(ZN2))
+    #print("I made my mind and I will print " + str(ZN2))
     return ZN2
         
     
@@ -45,19 +44,19 @@ def firstLineCorrection(inputForNInitiation):
     
 
 for N in sys.stdin:
-    print("itIsTheFirstLine is " + str(itIsTheFirstLine))
+    #print("itIsTheFirstLine is " + str(itIsTheFirstLine))
     if itIsTheFirstLine == True:
         firstLineCorrection(N)
     else:
         if N < setN:
             result = functionZ(int(N))
-            print("the setN now is " + str(setN))
-            print ("the N now is " + str(N))
-            print("The result is " + str(result))
+            #print("the setN now is " + str(setN))
+            #print ("the N now is " + str(N))
+            print(str(result))
         else:
             result = functionZ(int(N))
-            print("The result is " + str(result))
-            print("That's all folks!")
+            print(str(result))
+            #print("That's all folks!")
             #here i need to kill the function
         
 
